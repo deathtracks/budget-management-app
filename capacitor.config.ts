806916c58ca63fac2fca64e65f004de2c77c8b82
deathtracks <antoine.doste@hotmail.fr>
2021-07-19 10:22:17 +0200

    Deployed on android
    Made all setup for deploing on android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fr.Nebre.budget-management-app.',
+  appName: 'budget-management-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCCrJQR4g3hUOWOZ1HUprdaKGo0rIIdjek',
+    authDomain: 'budget-management-app-41b91.firebaseapp.com',
+    projectId: 'budget-management-app-41b91',
+    storageBucket: 'budget-management-app-41b91.appspot.com',
+    messagingSenderId: '905384323941',
+    appId: '1:905384323941:web:b1ad5638c9aa9e9afd8f8c',
+    measurementId: 'G-JT1VX7G6M0'
+  }
 };
