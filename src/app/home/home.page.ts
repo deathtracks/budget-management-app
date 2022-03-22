56d2606808200a14a516021398ea56b0055c3008
deathtracks <antoine.doste@hotmail.fr>
2022-03-22 23:01:13 +0100

    Modified the button at the bottom

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { IonRouterOutlet } from '@ionic/angular';
+import { AnimationController, ModalController } from '@ionic/angular';
 import { AddMonthComponent } from './add-month/add-month.component';
 
 @Component({
@@ -10,12 +9,19 @@ import { AddMonthComponent } from './add-month/add-month.component';
 })
 export class HomePage implements OnInit {
 
+  private main: HTMLDivElement;
+  private btn1: HTMLDivElement;
+  private btn2: HTMLDivElement;
+  private menuDisplay: boolean;
   constructor(
     public modalControler: ModalController,
-    private routerOutlet: IonRouterOutlet
+    private animationCtrl: AnimationController
   ) { }
 
   ngOnInit() {
+    this.main = document.getElementById('main-btn') as HTMLDivElement;
+    this.btn1 = document.getElementById('float-btn-1') as HTMLDivElement;
+    this.btn2 = document.getElementById('float-btn-2') as HTMLDivElement;
   }
 
   async presentModal() {
@@ -25,6 +31,20 @@ export class HomePage implements OnInit {
       initialBreakpoint: 0.35
     });
     return await modal.present();
+  }
+
+  showBtn(){
+    this.menuDisplay = !this.menuDisplay;
+    if(this.menuDisplay){
+      this.main.classList.add('rotate');
+      this.btn1.classList.add('raise-1');
+      this.btn2.classList.add('raise-2');
+    } else {
+      this.main.classList.remove('rotate');
+      this.btn1.classList.remove('raise-1');
+      this.btn2.classList.remove('raise-2');
+    }
+    
   }
 
 }
