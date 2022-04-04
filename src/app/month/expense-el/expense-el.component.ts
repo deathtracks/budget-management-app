09e303ed4d3bbdbf7351b0c2a23a98822dca8f0f
deathtracks <antoine.doste@hotmail.fr>
2022-04-04 09:17:35 +0200

    Added gesture to element on expense list

--- src/app/month/expense-el/expense-el.component.ts
+++ src/app/month/expense-el/expense-el.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { GestureController } from '@ionic/angular';
 import { Expense } from 'src/app/class/base/expense';
 
 @Component({
@@ -9,8 +10,31 @@ import { Expense } from 'src/app/class/base/expense';
 export class ExpenseElComponent implements OnInit {
   @Input() e: Expense;
   @Input() sectionName: string;
-  constructor() { }
+  constructor(
+    private gestureCtrl: GestureController
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const i = document.getElementById('page-content') as HTMLDivElement;
+    const elementWidht = i.clientWidth;
+    const g = this.gestureCtrl.create({
+      el: i,
+      direction : 'x',
+      onMove: (detail) => {
+        if(Math.abs(detail.deltaX)>elementWidht/2){
+          if(detail.deltaX>0){
+            i.classList.add('content-to-right');
+          } else {
+            i.classList.add('content-to-left');
+          }
+        } else {
+          i.classList.remove('content-to-right');
+          i.classList.remove('content-to-left');
+        }
+      },
+      gestureName: ''
+    });
+    g.enable(true);
+  }
 
 }
