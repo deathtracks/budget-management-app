8bc7562d9dd7b78463ad8337da4a01da1fcefcdf
deathtracks <antoine.doste@hotmail.fr>
2022-04-05 10:21:23 +0200

    fixed the issues with gesture.

--- src/app/month/expense-el/expense-el.component.ts
+++ src/app/month/expense-el/expense-el.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { GestureController } from '@ionic/angular';
+import { AfterViewChecked, AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
+import { GestureController, ViewDidEnter, ViewWillEnter } from '@ionic/angular';
 import { Expense } from 'src/app/class/base/expense';
 
 @Component({
@@ -7,31 +7,45 @@ import { Expense } from 'src/app/class/base/expense';
   templateUrl: './expense-el.component.html',
   styleUrls: ['./expense-el.component.scss'],
 })
-export class ExpenseElComponent {
+export class ExpenseElComponent implements AfterViewChecked {
   @Input() e: Expense;
   @Input() sectionName: string;
   @Output() onEdit: EventEmitter<boolean> = new EventEmitter<boolean>();
   @Output() onDelete: EventEmitter<boolean> = new EventEmitter<boolean>();
-  mouse: { x: number; y: number; };
-  mouseClick: { x: number; y: number; };
+  @ViewChild('content', { static: true, read: ElementRef }) contentElement;
 
-  @HostListener('window:mousemove', ['$event'])
-  onMouseMove(event: MouseEvent){
-    this.mouse = {
-      x: event.clientX,
-      y: event.clientY
-    }
-  };
-
-  @HostListener('window:mousedown',['$event'])
-  onMouseDown(event: MouseEvent){
-    this.mouseClick = {
-      x: event.clientX,
-      y: event.clientY
-    }
+  constructor(
+    private gestureCtrl: GestureController
+  ) {
+    // console.log('construct');
   }
+  
 
-  constructor() { }
+  ngAfterViewChecked(): void {
+    // console.log('ionViewDidEnter');
+    const element = document.getElementById(`page-content-${this.e.name}`);
+    const elementWidth = element.clientWidth;
+    // console.log(element);
+    const g = this.gestureCtrl.create({
+      el : element,
+      gestureName: '',
+      onMove: (details)=>{
+        if(Math.abs(details.deltaX)>elementWidth/4){
+          if(details.deltaX>0){
+            element.classList.remove('content-to-left');
+            element.classList.add('content-to-right');
+          } else {
+            element.classList.remove('content-to-right');
+            element.classList.add('content-to-left');
+          }
+        } else {
+          element.classList.remove('content-to-right');
+          element.classList.remove('content-to-left');
+        }
+      },
+    },true)
+    g.enable();
+  }
 
   
   public editBtn(){
